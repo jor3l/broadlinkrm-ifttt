dbd5b3d75b4137b36c0f6cdddf5e6c7b3bbfa2d5
srijansaxena11 <34964694+srijansaxena11@users.noreply.github.com>
2021-04-10 18:07:50 +0530

    uuid/v4 not working
    More details: https://github.com/uuidjs/uuid/blob/master/CHANGELOG.md#800-2020-04-29

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-const uuid = require('uuid/v4');
+//const uuid = require('uuid/v4');
+const { v4: uuid } = require('uuid');
 const request = require('request');
 const macRegExp = /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/;
 
